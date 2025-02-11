abf1034eae1c0db29b83310113f42dfd11dc9de4
Harsh <harshbhogade4485@gmail.com>
2025-02-11 12:58:31 +0530

    Initial commit - Timestamp Java program

---
+++ TimestampPrinter.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+
+public class TimestampPrinter {
+    public static void main(String[] args) {
+        System.out.println("Current Timestamp: " + LocalDateTime.now());
+    }
+}
---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+javac TimestampPrinter.java
+java TimestampPrinter
