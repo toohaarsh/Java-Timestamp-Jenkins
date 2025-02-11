d1442b367f7f1423abaeab629b6aa24cf0f91c1a
Harsh <harshbhogade4485@gmail.com>
2025-02-11 13:36:28 +0530

    Updated timestamp format

--- TimestampPrinter.java
+++ TimestampPrinter.java
@@ -2,6 +2,6 @@ import java.time.LocalDateTime;
 
 public class TimestampPrinter {
     public static void main(String[] args) {
-        System.out.println("Current Timestamp: " + LocalDateTime.now());
+        System.out.println("Change -1 for Timestamp: " + LocalDateTime.now());
     }
 }
